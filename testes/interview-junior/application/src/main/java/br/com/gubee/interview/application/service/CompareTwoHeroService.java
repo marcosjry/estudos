a5726e400f92ed25480b7ff6cc4dd28192149b01
Marcos Antônio <marcosjunior.me.009@gmail.com>
2025-07-29 15:58:26 -0300

    refactor: usecase agora usa lógica de negócio do domínio

--- testes/interview-junior/application/src/main/java/br/com/gubee/interview/application/service/CompareTwoHeroService.java
+++ testes/interview-junior/application/src/main/java/br/com/gubee/interview/application/service/CompareTwoHeroService.java
@@ -2,11 +2,9 @@ package br.com.gubee.interview.application.service;
 
 import br.com.gubee.interview.application.port.in.CompareTwoHeroUseCase;
 import br.com.gubee.interview.application.port.out.HeroQueryPort;
-import br.com.gubee.interview.application.port.out.PowerStatsQueryPort;
 import br.com.gubee.interview.domain.model.Comparison;
-import br.com.gubee.interview.domain.model.ComparisonResult;
+import br.com.gubee.interview.application.port.in.ComparisonResult;
 import br.com.gubee.interview.domain.model.Hero;
-import br.com.gubee.interview.domain.model.PowerStats;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,27 +15,22 @@ import java.util.UUID;
 public class CompareTwoHeroService implements CompareTwoHeroUseCase {
 
     private final HeroQueryPort heroQueryPort;
-    private final PowerStatsQueryPort powerStatsQueryPort;
 
     @Override
     public ComparisonResult compare(UUID heroOneId, UUID heroTwoId) {
+
         Hero heroOne = heroQueryPort.findById(heroOneId)
                 .orElseThrow(() -> new IllegalArgumentException("Hero one not found"));
         Hero heroTwo = heroQueryPort.findById(heroTwoId)
                 .orElseThrow(() -> new IllegalArgumentException("Hero two not found"));
 
-        PowerStats powerStatsOne = powerStatsQueryPort.findById(heroOne.getPowerStatsId())
-                .orElseThrow(() -> new IllegalArgumentException("PowerStats for hero one not found"));
-        PowerStats powerStatsTwo = powerStatsQueryPort.findById(heroTwo.getPowerStatsId())
-                .orElseThrow(() -> new IllegalArgumentException("PowerStats for hero two not found"));
+        Comparison comparison = heroOne.compareStatsWith(heroTwo);
 
-        Comparison comparison = new Comparison(
-                powerStatsOne.getAgility() - powerStatsTwo.getAgility(),
-                powerStatsOne.getDexterity() - powerStatsTwo.getDexterity(),
-                powerStatsOne.getStrength() - powerStatsTwo.getStrength(),
-                powerStatsOne.getIntelligence() - powerStatsTwo.getIntelligence()
-        );
+        String winnerHeroName =  heroOne.getPowerStats().getTotal() > heroTwo.getPowerStats().getTotal() ? heroOne.getName() : heroTwo.getName();
 
-        return new ComparisonResult(heroOne, heroTwo, comparison);
+        if (heroTwo.getPowerStats().getTotal() == heroTwo.getPowerStats().getTotal())
+            winnerHeroName = "DRAW";
+
+        return new ComparisonResult(heroOne, heroTwo, comparison, winnerHeroName);
     }
 }
