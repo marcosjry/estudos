c531eaa3e87c0e3d6563a1019c9630b887a14829
Marcos Antônio <marcosjunior.me.009@gmail.com>
2025-08-07 15:22:16 -0300

    feat: adicionando classe de dominio

---
+++ projeto-poc/order-producer/src/main/java/com/gubee/poc/domain/model/Order.java
@@ -0,0 +1,36 @@
+package com.gubee.poc.domain.model;
+
+public class Order {
+
+    public String id;
+    public String product;
+    public int quantity;
+
+    public Order() {}
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    
+}
